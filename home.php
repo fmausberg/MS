0679d106ad2610e5b011486b4e7b56349f0acba2
fmausberg <35453008+fmausberg@users.noreply.github.com>
2018-01-16 15:00:36 +0000

    Create home.php

---
+++ home.php
@@ -0,0 +1,20 @@
+<!doctypr html>
+<html>
+
+	<head>
+		<h1>Head Überschrift</h1>
+	</head>
+
+	<body>
+		<h1>Erste Überschrift</h1>
+			<div id="hauptmenü">
+				<h2>Erste Unter-Überschrift</h2>
+				<p>Text, der unter der ersten Unter-Überschrift steht.</p>
+			</div>
+			<div id="zweitbereich">
+				<h2>Zweite Unter-Überschrift</h2>
+				<p>Hier steht der Text unter der zweiten Unter-Überschrift</p>
+			</div>
+	</body>
+	
+</html>
